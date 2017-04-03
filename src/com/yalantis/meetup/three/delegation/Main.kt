b25ee051ef2f8f796c091f1c7198712f1e51bf44
ihor <kuchernkoigor@gmail.com>
2017-04-03 15:40:22 +0300

    multiple delegation

---
+++ src/com/yalantis/meetup/three/delegation/Main.kt
@@ -0,0 +1,27 @@
+package com.yalantis.meetup.three.delegation
+
+/**
+ * Created by ihor on 03.04.17.
+ */
+
+interface Nameable {
+    var name: String
+}
+
+class JackName : Nameable {
+    override var name: String = "Jack"
+}
+
+class LongRunner: Runnable {
+    override fun run() {
+        println("long")
+    }
+}
+
+class Person(name: Nameable, runner: Runnable): Nameable by name, Runnable by runner
+
+fun main(args: Array<String>) {
+    val person = Person(JackName(), LongRunner())
+    println(person.name)
+    person.run()
+}
